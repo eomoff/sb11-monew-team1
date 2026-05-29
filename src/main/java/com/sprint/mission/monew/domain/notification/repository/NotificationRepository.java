dc17f4c09d4799b4b84dcb1789279b8b54eab963
eomoff <eomoff@gmail.com>
2026-05-29 09:34:55 +0900

    test(green): findByIdAndUserId 구현으로 존재하지 않는 알림 ID 조회 테스트 통과

--- src/main/java/com/sprint/mission/monew/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/sprint/mission/monew/domain/notification/repository/NotificationRepository.java
@@ -2,11 +2,14 @@ package com.sprint.mission.monew.domain.notification.repository;
 
 import com.sprint.mission.monew.domain.notification.entity.Notification;
 import com.sprint.mission.monew.domain.notification.repository.querydsl.NotificationCustomRepository;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID>,
     NotificationCustomRepository {
+
+  Optional<Notification> findByIdAndUserId(UUID id, UUID userId);
 
   long countByUserIdAndConfirmedAtIsNull(UUID userId);
 }
