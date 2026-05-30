3795ba2c36c5fa2915916a7842db2e306182a571
eomoff <eomoff@gmail.com>
2026-05-30 17:13:22 +0900

    test(green): InterestRepository.findMatchingInterests JPQL 쿼리 구현

--- src/main/java/com/sprint/mission/monew/domain/interest/repository/InterestRepository.java
+++ src/main/java/com/sprint/mission/monew/domain/interest/repository/InterestRepository.java
@@ -2,7 +2,19 @@ package com.sprint.mission.monew.domain.interest.repository;
 
 import com.sprint.mission.monew.domain.interest.entity.Interest;
 import com.sprint.mission.monew.domain.interest.repository.querydsl.InterestCustomRepository;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface InterestRepository extends JpaRepository<Interest, UUID>, InterestCustomRepository {}
+public interface InterestRepository extends JpaRepository<Interest, UUID>, InterestCustomRepository {
+
+  @Query("""
+      SELECT DISTINCT i FROM Interest i JOIN i.keywords k
+       WHERE :title LIKE CONCAT('%', k.keyword, '%')
+          OR :summary LIKE CONCAT('%', k.keyword, '%')
+      """)
+  List<Interest> findMatchingInterests(@Param("title") String title,
+      @Param("summary") String summary);
+}
